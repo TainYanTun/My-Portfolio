ad8cf5428e971c8db678f1724d76b285dce20162
TainYanTun <166300382+TainYanTun@users.noreply.github.com>
2025-06-28 21:15:28 +0700

    Added chatbot (AI api not implemented

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import About from '../components/About';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 import Contact from '../components/Contact';
+import ChatBot from "../components/Chatbot";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <ChatBot />
     </div>
   );
 }

51a41c009ab0df575a8c4b9d55e2465264168456
TainYanTun <166300382+TainYanTun@users.noreply.github.com>
2025-07-21 22:28:43 +0700

    Added SpeedInsights for Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -15,14 +16,37 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Derek's Portfolio",
   description: "A brief showcase of my skills, projects, and more.",
+  authors: [{ name: "Tain Yan Tun" }],
+  keywords: ["portfolio", "developer", "projects", "skills", "Derek","Tain Yan Tun,", "web developer", "software engineer", "student portfolio"],
+  openGraph: {
+    title: "Derek's Portfolio",
+    description: "A brief showcase of my skills, projects, and more.",
+    url: "https://portfolio-two-beta-hac3efbegb.vercel.app",
+    siteName: "Derek's Portfolio",
+    images: [
+      {
+        url: "/webicon.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
   icons: {
-    icon: "/webicon.png",             // Main browser tab icon
-    shortcut: "/webicon.png",         // Legacy shortcut
-    apple: "/webicon.png",            // iOS Safari
+    icon: "/webicon.png",
+    shortcut: "/webicon.png",
+    apple: "/webicon.png",
     other: {
       rel: "apple-touch-icon-precomposed",
       url: "/webicon.png",
     },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Derek's Portfolio",
+    description: "A brief showcase of my skills, projects, and more.",
+    images: ["/webicon.png"],
   },
 };
 
@@ -37,6 +61,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
